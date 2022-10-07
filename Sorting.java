0e778afb4267ebcaf75e8d39e179e85ac8246ff0
Samriddhii-Siingh <108535666+Samriddhii-Siingh@users.noreply.github.com>
2022-10-07 14:37:08 +0530

    Merge Sort by Samriddhi

---
+++ Sorting.java
@@ -0,0 +1,71 @@
+import java.util.Arrays;
+
+public class Sorting {
+	
+	void merge(int array[], int low, int mid, int high) {
+		int n1 = mid - low + 1;
+		int n2 = high - mid;
+		
+		int X[] = new int[n1];
+		int Y[] = new int[n2];
+		
+		for(int i=0; i<n1; i++) {
+			X[i] = array[low + i];
+		}
+		for(int j=0; j<n2; j++) {
+			Y[j] = array[mid + 1 + j];
+		}
+		
+		int i, j, k;
+		i = 0;
+		j = 0;
+		k = low;
+		
+		while(i<n1 && j<n2) {
+			if(X[i] <= Y[j]) {
+				array[k] = X[i];
+				i++;
+			} else {
+				array[k] = Y[j];
+				j++;
+			}
+			k++;
+		}
+		
+		while (i<n1) {
+			array[k] = X[i];
+			i++;
+			k++;
+		}
+		
+		while (j<n2) {
+			array[k] = Y[j];
+			j++;
+			k++;
+		}
+	}
+	
+	void mergeSort(int array[], int low, int high) {
+		if(low < high) {
+			int mid = (low + high)/2;
+			
+			mergeSort(array, low, mid);
+			mergeSort(array, mid+1, high);
+			
+			merge(array, low, mid, high);
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] array = { 90,10,-6,7,-1,0,35 };
+		Sorting ob = new Sorting();
+		
+		ob.mergeSort(array, 0, array.length-1);
+		
+		System.out.println("Sorted Array: ");
+		System.out.println(Arrays.toString(array));
+	}
+
+}
