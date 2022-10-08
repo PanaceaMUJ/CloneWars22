6f8ee485ca40e610a22cf5bb5d60e520413d37e8
Ojasvi01 <97157187+Ojasvi01@users.noreply.github.com>
2022-10-08 17:15:14 +0530

    Quick Sort by Ojasvi

---
+++ Quick_sort.java
@@ -0,0 +1,61 @@
+package oOPS;
+
+import java.util.Scanner;
+
+public class Quick_sort 
+	{
+	public static void main(String args[])
+    {
+		Scanner sc=new Scanner(System.in);
+		
+        int arr[] = new int[10];
+        int n = arr.length;
+        for(int i=0;i<n;i++)
+        {
+        	arr[i]=sc.nextInt();
+        }
+        Quick_sort ob = new Quick_sort();
+        ob.sort(arr, 0, n-1);
+ 
+        System.out.println("sorted array");
+        printArray(arr);
+    }
+	   
+	    int partition(int arr[], int low, int high)
+	    {
+	        int pivot = arr[high];
+	        int i = (low-1); 
+	        for (int j=low; j<high; j++)
+	        {
+	            if (arr[j] <= pivot)
+	            {
+	                i++;
+	                int temp = arr[i];
+	                arr[i] = arr[j];
+	                arr[j] = temp;
+	            }
+	        }
+	 
+	        int temp = arr[i+1];
+	        arr[i+1] = arr[high];
+	        arr[high] = temp;
+	 
+	        return i+1;
+	    }
+	    void sort(int arr[], int low, int high)
+	    {
+	        if (low < high)
+	        {
+	            int pi = partition(arr, low, high);
+	            sort(arr, low, pi-1);
+	            sort(arr, pi+1, high);
+	        }
+	    }
+	     static void printArray(int arr[])
+	    {
+	        int n = arr.length;
+	        for (int i=0; i<n; ++i)
+	            System.out.print(arr[i]+" ");
+	        System.out.println();
+	    }   
+}
