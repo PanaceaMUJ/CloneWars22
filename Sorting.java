ad6650beeed42a3c24e4fee5aa1eae2f5d763c5c
Samriddhii-Siingh <108535666+Samriddhii-Siingh@users.noreply.github.com>
2022-10-08 13:30:57 +0530

    Update Sorting.java

--- Sorting.java
+++ Sorting.java
@@ -59,7 +59,21 @@ public class Sorting {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int[] array = { 90,10,-6,7,-1,0,35 };
+		int n;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number of elements in array : ");
+		n = sc.nextInt();
+		
+		int[] array = new int[n];
+		
+		System.out.println("Enter the elements of array: ");
+		for(int i=0; i<n; i++) {
+			array[i] = sc.nextInt();
+		}
+		
+		System.out.println("Unsorted Array: ");
+		System.out.println(Arrays.toString(array));
+		
 		Sorting ob = new Sorting();
 		
 		ob.mergeSort(array, 0, array.length-1);
