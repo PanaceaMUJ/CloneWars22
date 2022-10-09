90042eb96b426e126dd06008dedc0b6203c65bb6
Ojasvi01 <97157187+Ojasvi01@users.noreply.github.com>
2022-10-09 19:09:43 +0530

    Update Quick_sort.java

--- Quick_sort.java
+++ Quick_sort.java
@@ -4,9 +4,11 @@ public class Quick_sort
 	{
 	public static void main(String args[])
     {
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter the size of array : ");
+	int n=sc.nextInt();
         int arr[] = new int[n];
+	System.out.println("Enter the elements in the array: ");
         for(int i=0;i<n;i++)
         {
         	arr[i]=sc.nextInt();
